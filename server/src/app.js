93eac6698d779fd301ffa2c0be9ba83145021648
Adarsh kumar <adarshkumar0128@gmail.com>
2025-07-05 16:28:02 +0530

    Create view and edit profile

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/database.js");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile.js");
 
 const PORT = process.env.PORT || 7777;
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/", authRouter);
+app.use("/", profileRouter);
 
 connectDB()
   .then(() => {
---
+++ server/src/utils/FormatUserResponse.js
@@ -0,0 +1,16 @@
+const formatUserResponse = (user) => ({
+  _id: user._id,
+  firstName: user.firstName,
+  lastName: user.lastName,
+  emailId: user.emailId,
+  age: user.age,
+  gender: user.gender,
+  photoUrl: user.photoUrl,
+  about: user.about,
+  preferences: user.preferences,
+  city: user.city,
+});
+
+module.exports = {
+  formatUserResponse,
+};
